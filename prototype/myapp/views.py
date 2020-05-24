349c1c379682e4f1684472783d76af7a2017b4a9
lucyli1995 <liluxi0609@gmail.com>
2020-05-24 13:08:22 +1000

    minor fix

--- prototype/myapp/views.py
+++ prototype/myapp/views.py
@@ -3,6 +3,39 @@ from django.views.decorators.http import require_http_methods
 from django.core import serializers
 from django.http import JsonResponse
 import json
+import socket
+
+def queryBarcode(barcode):
+    #make connection
+    HOST = '127.0.0.1'
+    PORT = 9000
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    #sending query message
+    msg = barcode
+    sock.send(msg.encode())
+    reply = sock.recv(1024).decode('utf-8')
+    print(reply)
+    #send ending message
+    end = "QUIT"
+    sock.send(end.encode())
+    return reply
+
+def queryPos():
+    #make connection
+    HOST = '127.0.0.1'
+    PORT = 9002
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    #send query message
+    msg = "UPDATE"
+    sock.send(msg.encode())
+    reply = sock.recv(1024).decode('utf-8')
+    print(reply)
+    #send ending message
+    end = "QUIT"
+    sock.send(end.encode())
+    return reply
 
 
 # Create your views here.
